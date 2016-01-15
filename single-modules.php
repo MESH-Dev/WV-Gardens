67fa48a940e86340f46d212e6b2754d05c47d0f6
robbiesh <sherrard.rob@gmail.com>
2016-01-15 09:58:54 -0500

    fix complete

--- single-modules.php
+++ single-modules.php
@@ -515,9 +515,7 @@ get_header(); ?>
         </div>
         <div class="row">
           <div class="six offset-by-three columns">
-            <div class="module-image">
-              <div class="final-image">
-              </div>
+            <div class="final-image">
             </div>
           </div>
         </div>
