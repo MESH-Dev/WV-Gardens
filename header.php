bc4523defbea55a3978af1e115ed32651d20453a
meshdesign <joshdodd@meshfresh.com>
2016-07-27 17:10:11 -0400

    delete student

--- header.php
+++ header.php
@@ -33,6 +33,11 @@
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/animate.css" />
+	<?php if(is_page()){ ?>
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/chartist.css" />
+
+	<?php } ?>
+	
 	
 
 	<!-- Favicons
